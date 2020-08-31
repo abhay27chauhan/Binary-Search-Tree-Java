9ecade499f2cf2d83e5e057e62a95f9a380486b2
Abhay <chauhan27.abhay@gmail.com>
2020-08-31 17:31:46 +0530

    add Node

---
+++ addNode.java
@@ -0,0 +1,66 @@
+class addNode{
+
+    public static class Node{
+        int data;
+        Node left; 
+        Node right;
+
+        Node(int data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static Node constructor(int[] arr, int lo, int hi){
+        if(lo>hi){
+            return null;
+        }
+        
+        int mid = (lo+hi)/2;
+        int data = arr[mid];
+
+        Node lc = constructor(arr, lo, mid-1);
+        Node rc = constructor(arr, mid+1, hi);
+
+        Node node = new Node(data, lc, rc);
+        return node;
+    }
+
+    public static Node addNode(Node node, int val){
+        if(node == null){
+            return new Node(val, null, null);
+        }
+
+        if(val<node.data){
+            node.left = addNode(node.left, val);
+        }else if(val> node.data){
+            node.right = addNode(node.right, val);
+        }else{
+
+        }
+
+        return node;
+    }
+
+    public static void display(Node node){
+        if(node == null){
+            return;
+        }
+
+        System.out.print(node.left == null ? "." : node.left.data);
+        System.out.print(" <- " + node.data + " -> ");
+        System.out.print(node.right == null ? "." : node.right.data);
+
+        System.out.println();
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void main(String[] args){
+        int[] arr = {12,25,37,50,62,75,87}; // sorted array
+        Node root = constructor(arr, 0, arr.length-1);
+        addNode(root, 60);
+        display(root);
+    } 
+}
